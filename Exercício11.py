66527e3f1db07ec1499529327ce3b2917ddb6200
igorarenari <igorarenari7@hotmail.com>
2020-11-12 18:02:40 -0300

    ResolveExercício11

---
+++ Exercício11.py
@@ -0,0 +1,4 @@
+l = float(input('Largura da parede: '))
+h = float(input('Altura da parede: '))
+a = l*h
+print('A área da parede é de {} e são necessários {}L de tinta para pintá-la'.format(l*h,a/2))
